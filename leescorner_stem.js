135ef090593c01f9c20e5603ce26a67eea0bc6f9
Clayton Kramer <clayton.kramer@gmail.com>
2017-03-30 23:05:56 -0400

    updated with better functions

--- leescorner_stem.js
+++ leescorner_stem.js
@@ -10,11 +10,18 @@
     };
 
     // Block function
-    ext.get_temp = function (location, callback) {
+    ext.getWeatherData = function (sensor, location, callback) {
 
         var station = '';
+        var sensorMap = {
+            'Temperature': 'temp_f',
+            'Humidity': 'humidity',
+            'Rain Fall': 'rain_gauge',
+            'Wind Direction': 'wind_direction',
+            'Wind Speed': 'wind_speed'
+        }
 
-        switch(location) {
+        switch (location) {
             case 'School':
                 station = 'wunderground';
                 break;
@@ -31,11 +38,26 @@
             dataType: 'json',
             success: function (data) {
                 // Parse JSON for the temperature data
-                var temperature = data['temp_f'];
-                callback(temperature);
+
+                callback(data[sensorMap[sensor]]);
             }
         });
 
+    };
+
+    // Block function
+    ext.getRainFall = function (callback) {
+
+        // Make AJAX call to Wundergound weather API
+        $.ajax({
+            url: 'https://weather.kramertechsolutions.com/api/weatherstation/rainfall/',
+            dataType: 'json',
+            success: function (data) {
+                // Parse JSON for the temperature data
+
+                callback(data['rain_fall']);
+            }
+        });
 
     };
 
@@ -43,9 +65,11 @@
     var descriptor = {
         blocks: [
             // Block type, name and function
-            ['R', 'Temperature for %m.location', 'get_temp', 'School']
+            ['R', '%m.sensor for %m.location', 'getWeatherData', 'Temperature', 'School'],
+            ['R', 'Rain fall for classroom', 'getRainFall']
         ],
         menus: {
+            sensor: ['Temperature', 'Humidity', 'Rain Fall', 'Wind Direction', 'Wind Speed'],
             location: ['School', 'Classroom']
         }
     };
